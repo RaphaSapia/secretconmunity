584ba13323eba0299e6d565dda6eb61f715a390a
RaphaelSapia <raphael-sapia@hotmail.com>
2025-03-23 21:15:07 -0300

    db atualização

--- secretcommunity/__init__.py
+++ secretcommunity/__init__.py
@@ -12,7 +12,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # URI do Banco de Dados (Prioriza a variável de ambiente DATABASE_URL)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'community.db')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '57024a46c4f67b94e911c0319fabe56e'
+
+# SECRET_KEY (Obrigatória)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+if not app.config['SECRET_KEY']:
+    raise ValueError("A variável de ambiente SECRET_KEY não está definida!")
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 database = SQLAlchemy(app)
@@ -27,6 +32,6 @@ migrate = Migrate(app, database)  # Inicialize o Flask-Migrate
 from secretcommunity import routes
 from secretcommunity import models
 
-# Cria as tabelas do banco de dados dentro do contexto da aplicação
-with app.app_context():
-    database.create_all()
+# Remova a criação do banco de dados
+# with app.app_context():
+#     database.create_all()
