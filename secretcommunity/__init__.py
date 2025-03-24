018ac9feb7674098f654844e3f06db0245985b5d
RaphaelSapia <raphael-sapia@hotmail.com>
2025-03-23 22:16:56 -0300

    db atualização

--- secretcommunity/__init__.py
+++ secretcommunity/__init__.py
@@ -3,22 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate  # Importe o Flask-Migrate
+import sqlalchemy
 
-app = Flask(__name__)
+app = Flask(__name__)  # Remova instance_relative_config=True
 
 # Diretório base do projeto
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# URI do Banco de Dados (Prioriza a variável de ambiente DATABASE_URL)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'community.db')
-
-# SECRET_KEY (Obrigatória)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-if not app.config['SECRET_KEY']:
-    raise ValueError("A variável de ambiente SECRET_KEY não está definida!")
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# URI do Banco de Dados (Aponta para a raiz do projeto)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'community.db')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '57024a46c4f67b94e911c0319fabe56e'
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'community.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -27,11 +26,17 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-migrate = Migrate(app, database)  # Inicialize o Flask-Migrate
+from secretcommunity import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+
+if not inspector.has_table('user'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Database created')
+else:
+    print('Database already exists')
 
 from secretcommunity import routes
 from secretcommunity import models
-
-# Remova a criação do banco de dados
-# with app.app_context():
-#     database.create_all()
--- tester.py
+++ tester.py
@@ -19,5 +19,5 @@
 # from secretcommunity import app, database
 # with app.app_context():
 #     database.drop_all()
-#     database.create_all()
+# #     database.create_all()
 
